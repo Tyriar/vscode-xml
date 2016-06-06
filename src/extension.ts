8503315ad9fc32c253eb88dc3e18abeb70ee3a05
Daniel Imms <daimms@microsoft.com>
2016-06-05 18:30:46 -0700

    Add trigger characters

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import * as vscode from 'vscode';
 import {XmlCompletionItemProvider} from './xmlCompletionItemProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.languages.registerCompletionItemProvider('xml', new XmlCompletionItemProvider()));
+  let completionItemProviuder = vscode.languages.registerCompletionItemProvider('xml', new XmlCompletionItemProvider(), '<', '"', ' ', '/');
+  context.subscriptions.push(completionItemProviuder);
 }
 
 export function deactivate() {
